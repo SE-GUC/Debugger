f557d64f2e474711e24caa95c5ef7278ad3de51e
MahaMekdad <maha_mekdad@outlook.com>
2019-02-27 23:07:20 +0200

    reorganizing the files

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const express = require('../node_modules/express')
+const express = require('express')
 
 const users = require('./Models/User')
 const events = require('./Models/Event')
+const AWGs = require('./Models/AWG')
+
 
 const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send(`<h1>Welcome to Book Store</h1>
-    <a href="/api/users">Users</a>
-    <a href="/api/books">Events</a>
+    res.send(`<h1>¡AWG!</h1>
+    <a href="/api/users">users</a>
+    <a href="/api/events">Events</a>
+    <a href="/api/AWGs">AWG</a>
+    <a href="/api/MUN">MUN</a>
     `);
 })
 
