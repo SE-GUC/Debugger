14fd751b4814cea2d112982909778cdc734a8f62
AminaSidky <amina.sidky@student.guc.edu.eg>
2019-03-02 14:14:11 +0200

    view or edit awg profile

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const express = require('express')
 
 const vgsUsers = require('./routes/api/vgs_users_m')
-
+const users = require('./routes/api/users')
 const app = express()
 
-// Inorder to be able to use the "req.body" statement.
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send(`<h1>¡AWG!</h1>
     <a href="/api/AWGs">AWGs</a>
     <a href="/api/MUN">MUN</a>
-    <a href="/api/VGS">VGS</a>`);
+    <a href="/api/VGS">VGS</a>
+    <a href="/api/profile">edit or view your profile</a>
+    `);
 })
 
 // Direct routes to appropriate files 
 app.use('/api/VGS' , vgsUsers)
 app.use('/api/VGS/application_form', vgsUsers)
 app.use('/api/VGS/application_form_view', vgsUsers)
+app.use('/api/profile', users)
+
 
 
 // Handling 404
@@ -25,5 +29,5 @@ app.use((req, res) => {
     res.status(404).send({err: 'We can not find what you are looking for'});
 })
 
- const port= process.env.PORT || 3000;
+ const port= process.env.PORT || 4000;
  app.listen(port, () => console.log(`${port} is live and running...`))
