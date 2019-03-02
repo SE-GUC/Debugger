6fa4b24f887b89e6405e873377ac6129cc48381b
SherryAshraf <sherryashraf7@icloud.com>
2019-03-02 19:55:01 +0200

    sherryuserstoriesandamina

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
     <a href="/api/MUN">MUN</a>
     <a href="/api/VGS">VGS</a>
     <a href="/api/TIQ">TIQ</a>
-    <a href="/api/awgs">About Clubss</a>
+    <a href="/api/awgs">Aboutt Clubss</a>
   
     <a href="/api/profile">edit or view your profile</a>
     `);
