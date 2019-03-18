cb35767a366b2a2caa377753f21dce6930bb00c4
MahaMekdad <maha_mekdad@outlook.com>
2019-03-18 19:17:08 +0200

    Adding the AWG att, and Vote Model

--- Models/AWG.js
+++ Models/AWG.js
@@ -2,11 +2,13 @@ const uuid= require('uuid')
 
 class AWG 
 {
-    constructor( clubDescription, clubName )
+    constructor( clubDescription, clubName, aboutUs, contactUs )
     {
         this.id = uuid.v4();
         this.clubDescription = clubDescription;
         this.clubName = clubName;
+        this.aboutUs = aboutUs;
+        this.contactUs = contactUs
     };
 };
 
