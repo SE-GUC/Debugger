ae5dd2e58c84bf5175fe1d88c9a1dcb817e584eb
osamaHaliem <osama.haliem@gmail.com>
2019-03-19 14:15:19 +0200

    send contact us message

--- Models/Message.js
+++ Models/Message.js
@@ -1,10 +1,11 @@
-// const uuid= require('uuid')
+const uuid= require('uuid')
 
 
 class Message 
 {
     constructor( name, email, message)
     {
+        this.id = uuid.v4();
         this.name =  name;
         this.email = email;
         this.message = message;
