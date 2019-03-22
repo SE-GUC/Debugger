634a1707c33e82c367af0785478bb99d7f052f73
AminaSidky <amina.sidky@student.guc.edu.eg>
2019-03-22 20:25:37 +0200

    Add-user-feature-AWG

--- Models/User.js
+++ Models/User.js
@@ -20,3 +20,37 @@ class User
 };
 
 module.exports = User
+const express = require('express')
+const mongoose = require('mongoose')
+app.post('/api/Users/', (req, res) => {
+    const id = req.body.id
+    const name = req.body.name
+    const phoneNumber = req.body.phoneNumber
+    const email = req.body.email
+    const password = req.body.password
+    const birthday = req.body.birthday
+    const studyYear = req.body.studyYear
+    const modeOfTran = req.body.modeOfTran
+    const generalAddress = req.body.generalAddress
+    const clubName = req.body.clubName
+
+
+    
+    const User = {
+        id: id,
+        name: name,
+        phoneNumber: phoneNumber,
+        email: email,
+        password: password,
+        birthday: birthday,
+        studyYear: studyYear,
+        modeOfTran: modeOfTran,
+        generalAddress: generalAddress,
+        clubName: clubName
+
+
+
+    }
+    Users.push(user)
+    res.send(Users)
+})
