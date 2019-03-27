e9b8bfed7c28331bf618fa921c32028a8274f788
Omarghallab <omar.abdelrouf@student.guc.edu.eg>
2019-03-27 12:54:02 +0200

    posting announcemnt

--- index.js
+++ index.js
@@ -55,5 +55,7 @@ app.use((req, res) => {
     res.status(404).send({err: 'We can not find what you are looking for'});
 })
 
- const port= process.env.PORT || 3000;
+ const port= process.env.PORT || 5000;
  app.listen(port, () => console.log(`${port} is live and running...`))
+ 
+ 
--- routes/api/announcements.js
+++ routes/api/announcements.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose');
 
-const announce = require("../../Models/Announcement");
 
-const annoucem = [
-    new announce ("VGS" , "me" , "you" , null , "event" , null , "17" )
-];
+ router.post('/', async (req,res) => {
 
-router.get('/', (req, res) => res.json({data: annoucem}));
+    try {
+ 
+     const isValidated = validator.createValidation(req.body)
+ 
+     if (isValidated.error) return res.status(400).send({ error: isValidated.error.details[0].message })
+ 
+     const newAnnouncement = await Announcement.create(req.body)
+ 
+     res.json({msg:'announcement was posted successfully', data: newAnnouncement})
+ 
+    }
+ 
+    catch(error) {
+ 
+        // We will be handling the error later
+ 
+        console.log(error)
+ 
+    }  
+ 
+ })
 
 module.exports = router ;
