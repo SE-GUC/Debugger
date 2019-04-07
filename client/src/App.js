45916e981fc413570a7960a4d0df9f028f8da623
passantfathynasr <47832898+passantfathynasr@users.noreply.github.com>
2019-04-07 23:52:03 +0200

    passanttrial

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Users from './components/Users';
+
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    users : [{
+      id: 1,
+      title: 'user 1',
+      completed: false
+
+    },
+    {
+      id: 2,
+      title: 'user 2',
+      completed: true
+
+    },
+    {
+      id: 3,
+      title: 'user 3',
+      completed: false
+
+    },
+
+    ]
+  }
   render() {
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Users users = {this.state.users}/>
       </div>
     );
   }
