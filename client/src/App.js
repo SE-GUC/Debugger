783c7d2929cbd0c248af3db7f49e0df85244d649
MahaMekdad <maha_mekdad@outlook.com>
2019-04-11 22:19:32 +0200

    cleaning app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -6,58 +6,12 @@ import AppForm from "./components/AppForm";
 import Vote from "./components/Vote";
 
 class App extends Component {
-  //   this.
-
-  //   this.changingEmail = this.changingEmail.bind(this);
-  constructor(props) {
-    super(props);
-    this.state = {
-      VGS_User,
-      Applicants: []
-    };
-  }
-
-  vgs = new VGS_User();
-  //VGS_User = new  VGS_User('lol@jf.com',1,'tre2ak a5dar','eating',3,'NA')
-  switchNameHandler = () => {
-    console.log("Hola!");
-    console.log(this.VGS_User.email);
-  };
-
-  changingEmail = event => {
-    this.setState({
-      VGS_User: (this.vgs.email = event.target.value)
-    });
-    //  this.VGS_User.email=event.target.value;
-    //  console.log(VGS_User.email=event.target.value)
-  };
-
-  printobject(user) {
-    // console.log(user);
-    //this.setState({
-    //Applicants:
-    this.state.Applicants.concat(user);
-    //});
-
-    //Applicants.push(user)
-    console.log(this.Applicants);
-    console.log(this.Applicants.length);
-  }
 
   render() {
     return (
       <div className="App">
         
           <h1>Yelloo</h1>
-          {/* <button onClick={this.switchNameHandler}>Switch Name</button> */}
-          {/* <TestCom
-          user={this.vgs}
-          addObject={this.printobject}
-          Changed={this.changingEmail}
-        /> */}
-          {/* <TestCom obj={this.VGS_User} ass={this.VGS_User.email} Changed={this.changingEmail}></TestCom> */}
-          {/* <div>abc {VGS_User.email}</div> */}
-          {/* <div>{this.obj.name}</div> */}
           <a href="/AppForm">Application Form</a>
           <br/>
           <a href="/Vote">Create Vote</a>
@@ -65,7 +19,6 @@ class App extends Component {
           <Route path="/AppForm" component={AppForm} />
           <Route path="/Vote" component={Vote} />
           </Switch>
-          {/* <AppForm/> */}
         
       </div>
     );
