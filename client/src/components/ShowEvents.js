32321f6dd5d6696c359d45e0e19d44ddc132ddfd
MahaMekdad <maha_mekdad@outlook.com>
2019-04-15 00:32:13 +0200

    just removing some unused variables to clear react's warnings

--- client/src/components/ShowEvents.js
+++ client/src/components/ShowEvents.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
 import Events from "./Events" ;
-import FillEventForm from './FillEventForm';
-import EventsItem from './EventsItem' ;
 import axios from 'axios'
 
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
